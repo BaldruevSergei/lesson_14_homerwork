3c1c5db9ca6edb116f924a378651cad186e8e7e4
Sergei Baldruev <baldruev.sergey@gmail.com>
2024-01-29 15:57:59 +0800

    second

--- script/script.js
+++ script/script.js
@@ -1,35 +1,65 @@
 // Реализуйте форму добавления карточек пользователей. В форме должны быть поля (Имя, Фамилия и Возраст). При отправке формы должна формироваться карточка с данными пользователя в интерфейсе. Дизайн страницы и расположение элементов могут быть произвольными.
+// lesson_15 _honework. Доработать стили в прошлом домашнем задании (карточки пользователей). В выполненом задании должны присваиваться inline стили через js, а так же добавляться классы для карточек пользователя.
 
 const formNode = document.querySelector("#addForm");
 const datas = [];
-formNode.addEventListener("submit" , (event) => {
-    event.preventDefault();
-    const {lastname, name, age} = event.target;
+formNode.addEventListener("submit" , event => {
+    event.preventDefault(); 
+    // отмена дефолтного поведения - отправка формы 
+    // event.target ссылка на элемент (ноду) с которым произошло событие 
+    const {name, lastname, age} = event.target;
     const data = {
-        lastname: lastname.value,
         name: name.value,
+        lastname: lastname.value,          
         age: age.value
     };
     datas.push(data);
+    // вызываем перерисовку контейнера с пользоватлем
     rerender();
     console.log(datas);
-    event.target.reset(); 
+    event.target.reset(); // очиcтка полей ввода - target -
 })
 
-function getDataNode (lastname, name, age) {
+function getDataNode (name, lastname, age) {
     const container = document.createElement("div");
-    const lastnameNode = document.createElement("p");
     const nameNode = document.createElement("p");
+    const lastnameNode = document.createElement("p");
     const ageNode = document.createElement("p");
-    lastnameNode.innerText = lastname;
+    
     nameNode.innerText = name;
+    lastnameNode.innerText = lastname;
     ageNode.innerText = age;
-    container.append(lastnameNode, nameNode, ageNode);
+    container.style.border = "1px solid #ccc";
+    container.style.padding = "10px";
+    container.style.margin = "10px";   
+    
+    container.classList.add("userCard");
+    
+    container.append(nameNode, lastnameNode,  ageNode);    
     return container;
 
 }
 function rerender(){
-    const container = document.querySelector("#dataContainer");
+    const container = document.querySelector(".dataContainer");
     container.innerText = '';
-    datas.forEach(({lastname, name, age})=> container.append(getDataNode(lastname, name, age)))
+    datas.forEach(({name, lastname, age})=> container.append(getDataNode(name, lastname, age)))
+    
 }
+//  datas.forEach(({name, lastname, age})=> container.append(getDataNode(name, 
+/* {
+    name:
+    lastname:
+    age: 45
+}
+    name:Сергей
+    lastname: Бальдруев
+    age: 45
+возвращает     
+getDataNode(name, lastname, age)
+<div> 
+    <p>Сергей<p>
+    <p>Бальдруев<p>
+    <p>45<p>
+</div>
+    ========= 
+    Добавление в контейнер */
